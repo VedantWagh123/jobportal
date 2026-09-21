edb5ad1e133e179332c5ed01f3c30d5081b070dd
VedantWagh123 <vedantwaghwagh@gmail.com>
2026-09-22 01:36:26 +0530

    Auto-create uploads directory in multer to prevent ENOENT

--- server/config/multer.js
+++ server/config/multer.js
@@ -1,8 +1,12 @@
 import multer from "multer";
 import path from "path";
+import fs from "fs";
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
+        if (!fs.existsSync('uploads')) {
+            fs.mkdirSync('uploads', { recursive: true });
+        }
         cb(null, 'uploads/');
     },
     filename: function (req, file, cb) {
